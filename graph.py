e8f60852c5f387913997e24c9c5b9de2c0409988
Joe Gordon <joe.gordon0@gmail.com>
2014-10-02 20:19:03 -0700

    Add apache2 header
    and do a bit of cleanup

--- graph.py
+++ graph.py
@@ -1,4 +1,20 @@
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+
 import pydot
+
+# code initially based off of
+# http://pythonhaven.wordpress.com/2009/12/09/generating_graphs_with_pydot/
 
 graph = pydot.Dot(graph_type='digraph')
 
@@ -23,9 +39,9 @@ graph.add_edge(pydot.Edge(s['nova'],
                           color=REQUIRES,
                           label="keystomiddleware"))
 graph.add_edge(pydot.Edge(s['nova'],
-                           s['glance'],
-                           color=REQUIRES,
-                           label='glanceclient'))
+                          s['glance'],
+                          color=REQUIRES,
+                          label='glanceclient'))
 graph.add_edge(pydot.Edge(s['nova'],
                           s['glance'],
                           color=DEPENDSON))
@@ -36,8 +52,4 @@ graph.add_edge(pydot.Edge(s['glance'],
                           label="keystomiddleware"))
 
 
-
-# and we are done
 graph.write_png('OpenStack.png')
-
-# this is too good to be true!
